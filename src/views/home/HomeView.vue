cc4599a94aad1a031560acba6f81132e81e2407f
macarena <macarena@blockinar.com>
2023-12-05 23:27:28 -0300

    Add home background image

---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    <HelloWorld msg="Star Wars"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped lang="scss" src="./styles.scss"></style>
+
