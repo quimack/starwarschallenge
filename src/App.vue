a9e9b1acec271a02a11fdb99cb656d8dd42dc6de
macarena <macarena@blockinar.com>
2023-12-05 23:26:44 -0300

    Add StarJediHollow font

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="bckgr">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
-<style>
-#app {
+<style lang=scss>
+@import '@/assets/_variables.scss';
+
+@font-face {
+  font-family: "star-jedi-hollow";
+  src: url("@/assets/fonts/StarJediHollow.otf") format("opentype");
+}
+
+html,
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $text-200;
+  margin: 0;
+  background-color: $bg-100;
+  overflow-x: hidden;
+}
+
+.bckgr {
+  width: 100vw;
+  height: 100vh;
+  background: center / cover url(@/assets/images/sw-wallpaper.jpg) no-repeat;
 }
 
 nav {
-  padding: 30px;
+  padding: 1em;
+  font-family: 'star-jedi-hollow';
+  font-size: 1.4em;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: $primary-300;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: $primary-100;
 }
 </style>
